e66202a42d589d27d615889a81d1c8206c1730fd
Igor Jorge <igorjplino@outlook.com>
2023-08-15 08:48:31 +0100

    Refactor to start to using repository pattern

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,32 +1,31 @@
 using Core.Entities;
-using Infrastructue.Data;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    private readonly IProductRepository _productRepository;
+
+    public ProductsController(IProductRepository productRepository)
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
-        {
-            _context = context;
-        }
+        _productRepository = productRepository;
+    }
 
-        [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
-        {
-            var products = await _context.Products.ToListAsync();
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        var products = await _productRepository.GetProductsAsync();
 
-            return products;
-        }
+        return Ok(products);
+    }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
-        {
-            return await _context.Products.FindAsync(id);
-        }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        return await _productRepository.GetProductByIdAsync(id);
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
+using Core.Interfaces;
 using Infrastructue.Data;
+using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +15,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 

dee407dd2d3a0c8fd59df46b3c03ea2e0c60885e
Igor Jorge <igorjplino@gmail.com>
2023-11-18 12:21:28 +0000

    End of Section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -27,9 +27,13 @@ export class ShopService {
 
     params = params.append("sort", shopParams.sort);
     params = params.append("pageIndex", shopParams.pageNumber);
-    params = params.append("pageSize", 3);
+    params = params.append("pageSize", shopParams.pageSize);
 
     return this.http.get<Pagination<Product>>(this.baseUrl + 'products', { params });
+  }
+
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {

ba32376f53cb63e0924616c197c7233edbbc4837
Igor Jorge <igorjplino@gmail.com>
2024-02-11 11:56:44 +0000

    End of section 15

---
+++ API/Extensions/UserManagerExtentions.cs
@@ -0,0 +1,30 @@
+﻿using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace API.Extentions;
+
+public static class UserManagerExtentions
+{
+    public static async Task<AppUser> FindUserByClaimsPrincipleWithAddressAsync(
+        this UserManager<AppUser> userManager,
+        ClaimsPrincipal user)
+    {
+        var email = user.FindFirstValue(ClaimTypes.Email);
+
+        return await userManager.Users
+            .Include(x => x.Address)
+            .SingleOrDefaultAsync(x => x.Email == email);
+    }
+
+    public static async Task<AppUser> FindUserByEmailFromClaimsPrinciple(
+        this UserManager<AppUser> userManager,
+        ClaimsPrincipal user)
+    {
+        var email = user.FindFirstValue(ClaimTypes.Email);
+
+        return await userManager.Users
+            .SingleOrDefaultAsync(x => x.Email == email);
+    }
+}
